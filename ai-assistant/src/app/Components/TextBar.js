162b29670291c3b6bba8bd39d58dd5e9c0a9024b
Traorasp <miguelsolanopaz@outlook.com>
2024-05-20 18:13:35 -0400

    Added styling to input bar

--- ai-assistant/src/app/Components/TextBar.js
+++ ai-assistant/src/app/Components/TextBar.js
@@ -1,6 +1,8 @@
+import Image from "next/image"
+
 const TextBar = () => {
     return (
-        <div>
+        <div className="border border-black bg-violet-100 px-2 flex justify-between rounded-full">
             <textarea 
                 id="promptInput" 
                 name="prompt" 
@@ -8,9 +10,12 @@ const TextBar = () => {
                 autoCorrect="on" 
                 autoFocus="True"
                 maxLength={3000}
+                className=" resize-none w-full mx-2 p-2 outline-none bg-transparent"
                 >
-                
             </textarea>
+            <button>
+                <Image className="grow" src="/images/arrow.svg" alt="Send arrow" width={35} height={35}/>
+            </button>
         </div>
     )
 }
