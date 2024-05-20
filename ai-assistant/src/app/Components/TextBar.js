85d28a1c58b4403f66353b948df3699b0431707a
Traorasp <miguelsolanopaz@outlook.com>
2024-05-20 18:35:55 -0400

    Made it so inputbar can add messages

--- ai-assistant/src/app/Components/TextBar.js
+++ ai-assistant/src/app/Components/TextBar.js
@@ -1,6 +1,27 @@
 import Image from "next/image"
+import { useEffect, useState } from "react"
 
-const TextBar = () => {
+const TextBar = (prop) => {
+
+    const {addMessage} = prop
+
+    const [prompt, setPrompt] = useState("")
+    const changePrompt = (e) => setPrompt(e.target.value)
+
+    useEffect(() => {
+
+    }, [prompt])
+
+    const sendPrompt = () => {
+        //Display propmt on message board and clear bar
+        addMessage(prompt)
+        const inputBar = document.getElementById("promptInput")
+        inputBar.value = "" 
+        
+        //Do API stuff
+
+    }
+
     return (
         <div className="border border-black bg-violet-100 px-2 flex justify-between rounded-full">
             <textarea 
@@ -10,10 +31,12 @@ const TextBar = () => {
                 autoCorrect="on" 
                 autoFocus="True"
                 maxLength={3000}
+                onChange={changePrompt}
                 className=" resize-none w-full mx-2 p-2 outline-none bg-transparent"
                 >
+                {prompt}
             </textarea>
-            <button>
+            <button onClick={sendPrompt}>
                 <Image className="grow" src="/images/arrow.svg" alt="Send arrow" width={35} height={35}/>
             </button>
         </div>
--- ai-assistant/src/app/page.js
+++ ai-assistant/src/app/page.js
@@ -1,14 +1,28 @@
+'use client'
+
+
+import { useEffect, useState } from "react";
 import MessageBoard from "./Components/MessageBoard"
 import TextBar from "./Components/TextBar"
 
 export default function Home() {
+
+  const [messages, setMessages] = useState([])
+  const addMessage = (message) => {
+    setMessages([...messages, message])
+  }
+
+  useEffect(() => {
+    console.log(messages)
+  }, [messages])
+
   return (
     <main className="flex flex-col p-3">
       <div className="h-8">
         <h1 className=" font-bold text-3xl">AI ASSISTANT</h1>
       </div>
       <MessageBoard />
-      <TextBar />
+      <TextBar addMessage={addMessage}/>
       <div>
         <p className="font-thin italic">AI Assistant by Miguel Solano Paz</p>
       </div>
